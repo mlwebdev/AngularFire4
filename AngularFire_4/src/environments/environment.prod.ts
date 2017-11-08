a7630e357841599f40938222b85294d03d8c202f
mlwebdev <mlwebdeveloper@hotmail.com>
2017-11-08 15:44:03 -0500

    AngularFire4
    Angular 4 (CRUD) App using FireStore

---
+++ AngularFire_4/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAdWarhPEcCoynaV2_GQm5wT78bCImFDiM",
+    authDomain: "ng4firestore-1f543.firebaseapp.com",
+    databaseURL: "https://ng4firestore-1f543.firebaseio.com",
+    projectId: "ng4firestore-1f543",
+    storageBucket: "ng4firestore-1f543.appspot.com",
+    messagingSenderId: "465011932096"
+  }
+};
